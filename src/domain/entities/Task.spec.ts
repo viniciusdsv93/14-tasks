f2617e07e81b5749e1851f828ae0c13e3d59fb5b
Vinicius dos Santos Verissimo <viniciusdsv93@gmail.com>
2023-05-06 10:48:52 -0300

    test: ensure task entity return task id when getId method is called

--- src/domain/entities/Task.spec.ts
+++ src/domain/entities/Task.spec.ts
@@ -1,45 +1,62 @@
+import { randomUUID } from "crypto";
 import { Task } from "./Task";
 
 describe("Task Entity", () => {
 	test("Should return an instance of Task", () => {
+		const id = randomUUID();
 		const name = "Task sample";
 		const deadline = new Date(Date.now() + 86400 * 1000);
-		const newTask = new Task(name, deadline);
+		const newTask = new Task(id, name, deadline);
 		expect(newTask).toBeInstanceOf(Task);
 	});
 
 	test("Should throw an error if name provided is an empty string", () => {
+		const id = randomUUID();
 		const name = "";
 		const deadline = new Date(Date.now() + 86400 * 1000);
-		expect(() => new Task(name, deadline)).toThrowError("Task name cannot be empty");
+		expect(() => new Task(id, name, deadline)).toThrowError(
+			"Task name cannot be empty"
+		);
 	});
 
 	test("Should throw an error if deadline provided is on the past", () => {
+		const id = randomUUID();
 		const name = "Task sample";
 		const deadline = new Date(Date.now() - 86400 * 1000);
-		expect(() => new Task(name, deadline)).toThrowError(
+		expect(() => new Task(id, name, deadline)).toThrowError(
 			"Task deadline must be a future date"
 		);
 	});
 
-	test("Should return task name when getName method is called", () => {
+	test("Should return task id when getId method is called", () => {
+		const id = randomUUID();
 		const name = "Task sample";
 		const deadline = new Date(Date.now() + 86400 * 1000);
-		const newTask = new Task(name, deadline);
+		const newTask = new Task(id, name, deadline);
+		expect(newTask.getId()).toBe(id);
+	});
+
+	test("Should return task name when getName method is called", () => {
+		const id = randomUUID();
+		const name = "Task sample";
+		const deadline = new Date(Date.now() + 86400 * 1000);
+		const newTask = new Task(id, name, deadline);
 		expect(newTask.getName()).toBe(name);
 	});
 
 	test("Should return task deadline when getDeadline method is called", () => {
+		const id = randomUUID();
 		const name = "Task sample";
 		const deadline = new Date(Date.now() + 86400 * 1000);
-		const newTask = new Task(name, deadline);
+		const newTask = new Task(id, name, deadline);
 		expect(newTask.getDeadline()).toBe(deadline);
 	});
 
 	test("Should return false when getIsDone method is called after creating an instance", () => {
+		const id = randomUUID();
 		const name = "Task sample";
 		const deadline = new Date(Date.now() + 86400 * 1000);
-		const newTask = new Task(name, deadline);
+		const newTask = new Task(id, name, deadline);
 		expect(newTask.getIsDone()).toBe(false);
 	});
 });
--- src/domain/entities/Task.ts
+++ src/domain/entities/Task.ts
@@ -1,15 +1,21 @@
 export class Task {
+	private id: string;
 	private name: string;
 	private deadline: Date;
 	private isDone: boolean;
 
-	constructor(name: string, deadline: Date) {
+	constructor(id: string, name: string, deadline: Date) {
 		if (name === "") throw new Error("Task name cannot be empty");
 		if (deadline.getTime() <= Date.now())
 			throw new Error("Task deadline must be a future date");
+		this.id = id;
 		this.name = name;
 		this.deadline = deadline;
 		this.isDone = false;
+	}
+
+	getId() {
+		return this.id;
 	}
 
 	getName() {
